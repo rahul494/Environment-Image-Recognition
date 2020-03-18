df5742843339d5622a2bc895a06e02d2f0bc88a0
Rahul Sharma <rahul494@my.yorku.ca>
2020-03-18 19:45:58 -0400

    Update 3by3GridAnalysis.R

--- 3by3GridAnalysis.R
+++ 3by3GridAnalysis.R
@@ -1,44 +1,47 @@
 ######################################################################################
-# Name: Rahul Sharma  
+# Name: Rahul Sharma
 # Last Updated
 #
 ######################################################################################
 
 ## Read in the library and metadata
 library(jpeg)
-pm <- read.csv("C:\\Users\\Rahul\\Downloads\\photoMetaData.csv")
-n <- nrow(pm)
+img <-
+  readJPEG(paste0("C:\\Users\\Rahul\\Downloads\\columbiaImages\\", pm$name[1]))
 
-# the 9 predictors correspond to the median value of each portion of the 3x3 grid
-X <- matrix(NA, ncol=9, nrow=n)
-
-y <- as.numeric(pm$category == "outdoor-day")
-
-arr = c(1:10000)
+X <- matrix(NA, ncol = 9, nrow = n)
+arr <- c(1:1039680)
 
 cols <- list(c(), c(), c())
+grid.length <- ceiling(sqrt(length(arr) / 9))
 
 # divide pixel intensities into 3 columns
 for (c in 1:3) {
-  for (i in (3 * (c - 1) + 1):length(arr)) {
-    if (i %% 9 == 3 * (c - 1) + 1) {
-      cols[[c]] <- c(cols[[c]], arr[i:(i + (2))])
-    }
+  for (i in seq(
+    from = (grid.length * (c - 1) + 1),
+    to = length(arr),
+    by = sqrt(length(arr))
+  )) {
+    cols[[c]] <- c(cols[[c]], arr[i:(i + (grid.length - 1))])
   }
 }
+
 
 grid <- list(c(), c(), c(), c(), c(), c(), c() , c(), c())
 
 n <- 1
-for(i in 1:3){
-    grid[[n]] <- split(cols[[i]], ceiling(seq_along(cols[[i]])/(length(cols[[i]])/3)))[1]
-    n <- n + 1
-    grid[[n]] <- split(cols[[i]], ceiling(seq_along(cols[[i]])/(length(cols[[i]])/3)))[2]
-    n <- n + 1
-    grid[[n]] <- split(cols[[i]], ceiling(seq_along(cols[[i]])/(length(cols[[i]])/3)))[3]
-    n <- n + 1
+for (i in 1:3) {
+  grid[[n]] <-
+    split(cols[[i]], ceiling(seq_along(cols[[i]]) / (length(cols[[i]]) / 3)))[1]
+  n <- n + 1
+  grid[[n]] <-
+    split(cols[[i]], ceiling(seq_along(cols[[i]]) / (length(cols[[i]]) / 3)))[2]
+  n <- n + 1
+  grid[[n]] <-
+    split(cols[[i]], ceiling(seq_along(cols[[i]]) / (length(cols[[i]]) / 3)))[3]
+  n <- n + 1
 }
 
-for(i in 1:9){
-  X[1,i] <- median(unlist(grid[[i]]), na.rm = TRUE)
+for (i in 1:9) {
+  X[1, i] <- median(unlist(grid[[i]]), na.rm = TRUE)
 }
