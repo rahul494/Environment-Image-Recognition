3e7cce00c58858c790f397931c8b4864ccd834a4
Rahul Sharma <rahul494@my.yorku.ca>
2020-03-21 15:46:50 -0400

    Create MeanPixels.R

---
+++ Partitions/MeanPixels.R
@@ -0,0 +1,25 @@
+#######################################################################
+## Project: Environment Image Recognition
+## Script purpose: Summarize pixel intensities according to mean values 
+## Date: 2020-03-21
+## Author: Rahul Sharma
+#######################################################################
+
+## Read in the library and metadata
+library(jpeg)
+pm <- read.csv("C:\\Users\\Rahul\\Downloads\\photoMetaData.csv")
+n <- nrow(pm)
+
+trainFlag <- (runif(n) > 0.5) # generate 800 uniform numbers
+
+y <- as.numeric(pm$category == "outdoor-day")
+
+X <- matrix(NA, ncol = 3, nrow = n)
+for (j in 1:n) {
+  img <-
+    readJPEG(paste0("C:\\Users\\Rahul\\Downloads\\columbiaImages\\", pm$name[j]))
+  X[j, ] <- apply(img, 3, mean)
+  print(sprintf("%03d / %03d", j, n))
+}
+
+write.table(X, file="MeanPixelsSummary.txt", row.names=FALSE, col.names=FALSE)
