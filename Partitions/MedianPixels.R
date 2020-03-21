cd15540d3e7dc638887bdf2700db5cd758f6dae6
Rahul Sharma <rahul494@my.yorku.ca>
2020-03-21 15:52:23 -0400

    Create MedianPixels.R

---
+++ Partitions/MedianPixels.R
@@ -0,0 +1,20 @@
+#######################################################################
+## Project: Environment Image Recognition
+## Script purpose: Summarize pixel intensities according to median values 
+## Date: 2020-03-21
+## Author: Rahul Sharma
+#######################################################################
+
+## Read in the library and metadata
+library(jpeg)
+pm <- read.csv("C:\\Users\\Rahul\\Downloads\\photoMetaData.csv")
+n <- nrow(pm)
+
+X <- matrix(NA, ncol = 3, nrow = n)
+for (j in 1:n) {
+  img <- readJPEG(paste0("C:\\Users\\Rahul\\Downloads\\columbiaImages\\", pm$name[j]))
+  X[j, ] <- apply(img, 3, median)
+  print(sprintf("%03d / %03d", j, n))
+}
+
+write.table(X, file="MedianPixelsSummary.txt", row.names=FALSE, col.names=FALSE)
