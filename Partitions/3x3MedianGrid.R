0b64a59876b5d6720e9ac107950d4a38a1dd2b9b
Rahul Sharma <rahul494@my.yorku.ca>
2020-03-23 20:41:13 -0400

    Create 3x3MedianGrid.R

---
+++ Partitions/3x3MedianGrid.R
@@ -0,0 +1,55 @@
+#######################################################################
+## Project: Image Processing Methods for Environment Classification
+## Script purpose: Analyze and Summarize photos via 3x3 median median grid
+## Date: 2020-03-23
+## Author: Rahul Sharma
+#######################################################################
+
+## Read in the library and metadata
+library(jpeg)
+pm <- read.csv("C:\\Users\\Rahul\\Downloads\\photoMetaData.csv")
+gallery.size <- nrow(pm)
+
+X <- matrix(NA, ncol=27, nrow=gallery.size)
+
+#
+# M    
+# r   
+# c
+#
+# mat_split
+
+mat_split <- function(M, r, c){
+  nr <- ceiling(nrow(M)/r)
+  nc <- ceiling(ncol(M)/c)
+  newM <- matrix(NA, nr*r, nc*c)
+  newM[1:nrow(M), 1:ncol(M)] <- M
+  
+  div_k <- kronecker(matrix(seq_len(nr*nc), nr, byrow = TRUE), matrix(1, r, c))
+  matlist <- split(newM, div_k)
+  N <- length(matlist)
+  mats <- unlist(matlist)
+  dim(mats)<-c(r, c, N)
+  return(mats)
+}
+
+for (k in 1:gallery.size){
+  
+  img <- readJPEG(paste0("C:\\Users\\Rahul\\Downloads\\columbiaImages\\",pm$name[k]))
+  
+  for(i in 1:3){
+    X[k,(1+9*(i-1))] <- median(mat_split(img[,,i], round(dim(img[,,i])[1]/3), dim(img[,,i])[2])[,,1][,1:3])
+    X[k,(2+9*(i-1))] <- median(mat_split(img[,,i], round(dim(img[,,i])[1]/3), dim(img[,,i])[2])[,,1][,4:6])
+    X[k,(3+9*(i-1))] <- median(mat_split(img[,,i], round(dim(img[,,i])[1]/3), dim(img[,,i])[2])[,,1][,7:9])
+    X[k,(4+9*(i-1))] <- median(mat_split(img[,,i], round(dim(img[,,i])[1]/3), dim(img[,,i])[2])[,,2][,1:3])
+    X[k,(5+9*(i-1))] <- median(mat_split(img[,,i], round(dim(img[,,i])[1]/3), dim(img[,,i])[2])[,,2][,4:6])
+    X[k,(6+9*(i-1))] <- median(mat_split(img[,,i], round(dim(img[,,i])[1]/3), dim(img[,,i])[2])[,,2][,7:9])
+    X[k,(7+9*(i-1))] <- median(mat_split(img[,,i], round(dim(img[,,i])[1]/3), dim(img[,,i])[2])[,,3][,1:3])
+    X[k,(8+9*(i-1))] <- median(mat_split(img[,,i], round(dim(img[,,i])[1]/3), dim(img[,,i])[2])[,,3][,4:6])
+    X[k,(9+9*(i-1))] <- median(mat_split(img[,,i], round(dim(img[,,i])[1]/3), dim(img[,,i])[2])[,,3][,7:9])
+  }
+  
+  print(sprintf("%03d / %03d", k, gallery.size))
+}
+
+write.table(X, file="3x3MedianSummary.txt", row.names=FALSE, col.names=FALSE)
