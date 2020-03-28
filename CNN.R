515fe688a95b310eba6ea9708c1740db29f8a5df
Rahul Sharma <rahul494@my.yorku.ca>
2020-03-27 22:25:29 -0400

    Create CNN.R

---
+++ CNN.R
@@ -0,0 +1,87 @@
+# Convolutional Neural Networks
+
+# Load packages
+library(keras)
+library(EBImage)
+
+# Read Images
+train <- list()
+for(i in 1:700) train[[i]] <- readImage(paste0("C:\\Users\\Rahul\\Downloads\\columbiaImages\\",pm$name[i]))
+
+test <- list()
+for(i in 1:100) test[[i]] <- readImage(paste0("C:\\Users\\Rahul\\Downloads\\columbiaImages\\",pm$name[(i+700)]))
+  
+# Resize & combine
+str(train)
+for (i in 1:700) {train[[i]] <- resize(train[[i]], 100, 100)}
+for (i in 1:100) {test[[i]] <- resize(test[[i]], 100, 100)}
+
+train <- combine(train)
+test <- combine(test)
+
+# Reorder dimension
+train <- aperm(train, c(4, 1, 2, 3))
+test <- aperm(test, c(4, 1, 2, 3))
+
+# Response
+trainy <- as.numeric(pm$category == "outdoor-day")[1:700]
+testy <- as.numeric(pm$category == "outdoor-day")[701:800]
+
+# One hot encoding
+trainLabels <- to_categorical(trainy)
+testLabels <- to_categorical(testy)
+
+# Model
+model <- keras_model_sequential()
+
+model %>%
+  layer_conv_2d(filters = 16, 
+                kernel_size = c(3,3),
+                activation = 'relu',
+                input_shape = c(100, 100, 3)) %>%
+  layer_conv_2d(filters = 16,
+      kernel_size = c(3,3),
+      activation = 'relu') %>%
+  layer_max_pooling_2d(pool_size = c(2,2)) %>%
+  layer_dropout(rate = 0.25) %>%
+  layer_conv_2d(filters = 32,
+                kernel_size = c(3,3),
+                activation = 'relu') %>%
+  layer_conv_2d(filters = 32,
+                kernel_size = c(3,3),
+                activation = 'relu') %>%
+  layer_max_pooling_2d(pool_size = c(2,2)) %>%
+  layer_conv_2d(filters = 64,
+                kernel_size = c(3,3),
+                activation = 'relu') %>%
+  layer_conv_2d(filters = 64,
+                kernel_size = c(3,3),
+                activation = 'relu') %>%
+  layer_max_pooling_2d(pool_size = c(2,2)) %>%
+  layer_dropout(rate = 0.25) %>%
+  layer_flatten() %>%
+  layer_dense(units = 256, activation = 'relu') %>%
+  layer_dropout(rate=0.25) %>%
+  layer_dense(units = 2, activation = 'softmax')
+  
+  model %>% compile(
+    loss = loss_categorical_crossentropy,
+    optimizer = optimizer_adadelta(),
+    metrics = c('accuracy')
+  )
+
+batch_size <- 64
+epochs <- 32
+
+# Train model
+model %>% fit(
+  train, trainLabels,
+  batch_size = batch_size,
+  epochs = epochs,
+  validation_split = 0.2
+)
+
+# Evaluation & Prediction - train data
+model %>% evaluate(train, trainLabels)
+pred <- model %>% predict_classes(train)
+table(Predicted = pred, Actual = trainy)
