16ae513e5928b4a9e8c9afdb25fd49e59ddef2c7
Rahul Sharma <sharmarah494@gmail.com>
2020-04-05 20:09:16 -0400

    Update PenalizedLogisticRegression3x3.R

--- PenalizedLogisticRegression3x3.R
+++ PenalizedLogisticRegression3x3.R
@@ -4,11 +4,15 @@
 ## Date: 2020-04-05
 ## Author: Rahul Sharma
 #######################################################################
+
 library(glmnet)
+
+# Read in metadata and 3x3 pixel summaries
 pm <- read.csv("C:\\Users\\Rahul\\Downloads\\photoMetaData.csv")
 mean.3x3 <- read.table("C:\\Users\\Rahul\\Documents\\MATH 3333 Data analytics A hands-on approach\\Project\\3x3MeanSummary.txt", header = FALSE, sep = " ")
 median.3x3 <- read.table("C:\\Users\\Rahul\\Documents\\MATH 3333 Data analytics A hands-on approach\\Project\\3x3MedianSummary.txt", header = FALSE, sep = " ")
 
+# Drop observations that have null pixel values
 na.rows <- unique(which(is.na(median.3x3), arr.ind = TRUE)[, 1])
 median.3x3 <- median.3x3[-c(na.rows), ]
 y <- as.numeric(pm$category == "outdoor-day")
@@ -16,6 +20,8 @@ y <- y[-c(na.rows)]
 
 sum <- 0
 rep <- 10
+
+# repeat cross-validation 10 times
 for(i in 1:rep){
   trainFlag <- (runif(nrow(median.3x3)) > 0.5)
   cvfit = cv.glmnet(data.matrix(median.3x3), y, family = "binomial", type.measure = "class", alpha = 1, subset = trainFlag )
